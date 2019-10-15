fe2efdee747b99cbef7b8d7d7c7591493856ab92
Florian Hindenlang <florian.hindenlang@ipp.mpg.de>
2019-10-15 16:46:17 +0200

    introduced sbase_prof for separate basis for profiles in restart. currently still = X1_base_r.

--- src/readstate/readstate_vars.f90
+++ src/readstate/readstate_vars.f90
@@ -23,6 +23,7 @@ MODULE MODgvec_ReadState_Vars
 USE MODgvec_Globals,ONLY:wp
 USE MODgvec_sgrid,  ONLY: t_sgrid
 USE MODgvec_base,   ONLY: t_base
+USE MODgvec_sbase,  ONLY: t_sbase
 USE MODgvec_c_hmap, ONLY: c_hmap
 IMPLICIT NONE
 PUBLIC
@@ -31,6 +32,7 @@ PUBLIC
   INTEGER                   :: fileID_r,OutputLevel_r
   CLASS(c_hmap),ALLOCATABLE :: hmap_r                 !! container for global coordinate system
   TYPE(t_sgrid)             :: sgrid_r                !! container for the grid of X1,X2,LA
+  CLASS(t_sbase),ALLOCATABLE:: sbase_prof             !! container for base for profiles
   CLASS(t_base),ALLOCATABLE :: X1_base_r              !! container for base of X1
   CLASS(t_base),ALLOCATABLE :: X2_base_r              !! container for base of X2
   CLASS(t_base),ALLOCATABLE :: LA_base_r              !! container for base of LA
