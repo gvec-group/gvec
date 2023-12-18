10bf8115271272796a26fc5bc21816da6b9e1c75
Florian Hindenlang <florian.hindenlang@ipp.mpg.de>
2023-12-18 21:41:04 +0100

    update of transform_sfl: - the module now contains a class 't_transform_sfl' that can be initialized with any solution (before, restart solution was inbuilt). - The functionality remains the same, only interfaces have changed, because also hmap and profile evaluation functions have to be passed, as well as the base classes. - Due to the interface change, converters using straight-field line transform (gvec_to_hopr,gvec_to_castor3d,gvec_to_gene) had to be modified, without changing their final result.

--- src/gvec_to_gene/gvec_to_gene_vars.f90
+++ src/gvec_to_gene/gvec_to_gene_vars.f90
@@ -23,12 +23,14 @@
 MODULE MODgvec_gvec_to_gene_Vars
 ! MODULES
 USE MODgvec_Globals,ONLY:wp
+USE MODgvec_transform_sfl     ,ONLY: t_transform_sfl
 IMPLICIT NONE
 PUBLIC
 !-----------------------------------------------------------------------------------------------------------------------------------
 ! GLOBAL VARIABLES 
 INTEGER       :: SFLcoord            !! =0: 'old way' of PEST with newton iteration, =1: PEST, =2: Boozer
 INTEGER       :: factorSFL           !! factor of the SFL coordinate mode numbers over the number of GVEC modes in X1/X2/LA
+CLASS(t_transform_sfl),ALLOCATABLE :: trafoSFL
                                          
 !===================================================================================================================================
 END MODULE MODgvec_gvec_to_gene_Vars
