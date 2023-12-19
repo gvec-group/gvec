aea2995d6ecb16b62b1fa39cd3c497ef4dd291b3
Tiago Tamissa Ribeiro <ttr@ipp.mpg.de>
2023-12-19 11:00:03 +0000

    update .gitlab-ci.yml file to add regression tests between different commit hashes and/or casenames (e.g. between intel and gnu runs);

--- gitlab_regressions.py
+++ gitlab_regressions.py
@@ -27,23 +27,17 @@ parser = argparse.ArgumentParser(description='# Tool to compare regression resul
                                              '# example execution command:\n'
                                              '# python gitlab_regression.py execdir1 execdir2',\
                                  formatter_class=argparse.RawTextHelpFormatter)
-#t#parser.add_argument('builddir',type=str, 
-#t#                    help='Name of builddir from .gitlab.yml folder, mandatory last argument')
 parser.add_argument('execdir1',type=str,
                     help='Name of execdir1 from .gitlab.yml folder, mandatory last argument')
 parser.add_argument('execdir2',type=str,
                     help='Name of execdir2 from .gitlab.yml folder, mandatory last argument')
-#t#parser.add_argument('-execpre',type=str, default='',
-#t#                    help='command prefix to execute (like "mpirun -np 2", or "export OMP_NUM_THREADS=10;")')
 parser.add_argument('-case', type=str, default='0',    help="0 : DEFAULT, run all cases,\n" 
                                                             "1,2-4 : list of specific cases to run (without spaces!) ")
 
 args = parser.parse_args()
 
-#t#builddir=args.builddir
 execdir1=args.execdir1
 execdir2=args.execdir2
-#t#execpre=args.execpre
 
 cases = parse_range(args.case)
 
@@ -52,11 +46,8 @@ success= []
 
 
 print( '='*132 )
-#t#print("RUNNING GITLAB TESTS, builddir: %s, execdir: %s"%(builddir,execdir))
 print("RUNNING GITLAB TESTS, execdir1: %s, execdir2: %s"%(execdir1,execdir2))
 print( '='*132 )
-
-#t#os.chdir(execdir)
 
 refdir="../test-CI"
 referenceStateFile="REF_GITLAB_RUN_State_0000_00001000.dat"
@@ -69,40 +60,53 @@ caseID=1
 if(cases[0]==0 or (caseID in cases)) :
     casename="1st_simulation"
     print("running caseID %d ,%s ... " % (caseID,casename))
-    #t#execbin="../"+builddir+"/bin/gvec"
-    #t#param="../test-CI/gvec_shortrun.ini"
     finalStateFile="GITLAB_RUN_State_0000_00001000.dat"
-    logFile='logMinimizer_GITLAB_RUN_0000.csv'
     finalStateFile1=execdir1+"/"+finalStateFile
     finalStateFile2=execdir2+"/"+finalStateFile
-    print("final state file 1 is: %s" %finalStateFile1)
-    print(finalStateFile2)
+    print("Final state file 1 is: %s" % finalStateFile1)
+    print("Final state file 2 is: %s" % finalStateFile2)
+    logFile='logMinimizer_GITLAB_RUN_0000.csv'
+    logFile1=execdir1+"/"+logFile
+    logFile2=execdir2+"/"+logFile
+    print("Log file 1 is: %s" % logFile1)
+    print("Log file 2 is: %s" % logFile2)
+    runfailed=[]
     if(not os.path.isfile(finalStateFile1) or not os.path.isfile(finalStateFile2)):
         msg=("caseID: %d, %s test failed, final state file does not exist" % (caseID,casename))
         failed.extend([msg])
     else:
-        runfailed=[]
+        #stdout="std_out_"+casename+execdir1+"_vs_"+execdir2+"_statefile.txt"
+        #stderr="std_err_"+casename+execdir1+"_vs_"+execdir2+"_statefile.txt"
+        #os.system("touch " + stdout)
+        #os.system("touch " + stderr)
+        #checkerr = check_stderr(stderr)
         nodiff,msg = compare_by_numdiff(execdir1,finalStateFile,execdir2,finalStateFile,ignore_strings=['#'])
         msg=("caseID: %d, %s , %s" %(caseID,casename,msg))
         if (nodiff):
             success.extend([msg])
         else:
             runfailed.extend([msg])
+    if(not os.path.isfile(logFile1) or not os.path.isfile(logFile2)):
+        msg=("caseID: %d, %s test failed, log file does not exist" % (caseID,casename))
+        failed.extend([msg])
+    else:
+        #stdout="std_out_"+casename+execdir1+execdir2+"_logfile.txt"
+        #stderr="std_err_"+casename+execdir1+execdir2+"_logfile.txt"
         nodiff,msg = compare_by_numdiff(execdir1,logFile,execdir2,logFile,colcsv='1,3-')
         msg=("caseID: %d, %s , %s" %(caseID,casename,msg))
         if (nodiff):
             success.extend([msg])
         else:
             runfailed.extend([msg])
-        if(len(runfailed) > 0) :
-            for line in runfailed :
-                print( "!!!! ---> "+line )
-            msg=("caseID: %d, %s test failed!"  %(caseID,casename))
-            failed.extend(runfailed)
-        else:
-            msg=("caseID: %d, %s did execute successfully!"  %(caseID,casename))
-            success.extend([msg])
-            print(msg)
+    if(len(runfailed) > 0) :
+        for line in runfailed :
+            print( "!!!! ---> "+line )
+        msg=("caseID: %d, %s test failed!"  %(caseID,casename))
+        failed.extend(runfailed)
+    else:
+        msg=("caseID: %d, %s did execute successfully!"  %(caseID,casename))
+        success.extend([msg])
+        print(msg)
 
 
 #########################################################################
