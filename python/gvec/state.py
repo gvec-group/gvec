Addressed merge-request review. - bspl2gvec now handles both scipy B-splines and knot+coef vectors - the state `evaluate_profile` has an additional `deriv` parameter to handle derivatives - fixed a bug in the test_profile.py where the 4th derivative was wrongly computed and not tested. - deleted obsolete poly2bspl.f90 file.

python/gvec/state.py
@@ -406,7 +406,25 @@ class State:
     )  # -> Jac_h, dJac_h_dr, dJac_h_dt, dJac_h_dz
 
     @_assert_init
-    def evaluate_profile(self, quantity: str, rho: np.ndarray):
+    def evaluate_profile(self, quantity: str, rho: np.ndarray, deriv: int = 0):
+        """Evaluate 1D profiles at the provided positions of the radial coordinate rho.
+
+        Args:
+            quantity (str): name of the profile. Has to be either `iota`, `p`, `chi`, `Phi` or `PhiNorm`
+            rho (np.ndarray): Positions at the radial flux coordinate rho.
+            deriv (int, optional): Order of the derivative. Note that for some quantities not all derivatives can
+            be calculated, e.g. for `iota` and `p` the maximum is `deriv=4`. Defaults to 0.
+
+        Raises:
+            ValueError: If `quantity`is not a string.
+            ValueError: If an invalid quantity is provided.
+            NotImplementedError: If `deriv > 1` for `quantity="chi"`.
+            ValueError: If `rho` is not a 1D array.
+            ValueError: If `rho` is not in [0, 1].
+
+        Returns:
+            np.ndarray: profile values at `rho`.
+        """
         if not isinstance(quantity, str):
             raise ValueError("Quantity must be a string.")
         elif quantity not in [
@@ -424,41 +442,19 @@ class State:
         ]:
             raise ValueError(f"Unknown quantity: {quantity}")
 
-        rho = np.asfortranarray(rho, dtype=np.float64)
-        if rho.ndim != 1:
-            raise ValueError("rho must be a 1D array.")
-        if rho.max() > 1.0 or rho.min() < 0.0:
-            raise ValueError("rho must be in the range [0, 1].")
+        if "_prime" in quantity:
+            deriv += 1
+            quantity = quantity.replace("_prime", "")
 
-        result = np.zeros(rho.size, dtype=np.float64, order="F")
-        _post.evaluate_profile(rho.size, rho, quantity, result)
-        return result
+        if "_2prime" in quantity:
+            deriv += 2
+            quantity = quantity.replace("_2prime", "")
 
-    @_assert_init
-    def evaluate_profile_deriv(self, quantity: str, rho: np.ndarray, order: int):
-        """Evaluate the n-th derivative with respect to rho of the specified profile at position `rho`.
-
-        Args:
-            quantity (str): profile identifyer, has to be either `"iota"` or `"pres"`.
-            rho (np.ndarray): radial position(s).
-            order (int): order of the derivative with order in [0,4].
-
-        Raises:
-            ValueError: If `quantity` is not a string.
-            ValueError: If `quantity` is not `"iota"` or `"pres"`.
-            ValueError: If `rho` is not a 1D array.
-            ValueError: If `rho` is not in [0,1].
-
-        Returns:
-            np.ndarray: derivative values at `rho` positions.
-        """
-        if not isinstance(quantity, str):
-            raise ValueError("Quantity must be a string.")
-        elif quantity not in [
-            "iota",
-            "p",
-        ]:
-            raise ValueError(f"Unknown quantity: {quantity}")
+        if quantity == "chi" and deriv > 1:
+            raise NotImplementedError(
+                "chi derivative is not implemented!"
+                + f" Requestet deriv={deriv} but max. implemented deriv=1."
+            )
 
         rho = np.asfortranarray(rho, dtype=np.float64)
         if rho.ndim != 1:
@@ -467,7 +463,14 @@ class State:
             raise ValueError("rho must be in the range [0, 1].")
 
         result = np.zeros(rho.size, dtype=np.float64, order="F")
-        _post.evaluate_profile_deriv(rho.size, rho, order, quantity, result)
+
+        if quantity == "PhiNorm" and deriv == 2:
+            return 2 * np.ones(rho.size, dtype=np.float64, order="F")
+
+        if quantity in ["Phi", "PhiNorm"] and deriv > 2:
+            return result
+
+        _post.evaluate_profile(rho.size, rho, deriv, quantity, result)
         return result
 
     # === Boozer Potential === #

python/gvec/util.py
@@ -15,13 +15,12 @@ from pathlib import Path
 import re
 import shutil
 from numpy.typing import ArrayLike
+from typing import Literal
 
 try:
     from scipy.interpolate import BSpline
 except ImportError:
-    _has_scipy = False
-else:
-    _has_scipy = True
+    BSpline = None
 
 
 def adapt_parameter_file(source: str | Path, target: str | Path, **kwargs):
@@ -330,63 +329,47 @@ def np2gvec(a: ArrayLike) -> str:
     return a_str
 
 
-def scipy_bspl2gvec(which_profile: str, bspl, params={}) -> dict:
-    """Translates a scipy BSpline object into the necessary parameters utilized by the gvec parameter file.
-
-    Args:
-        which_profile (str): profile identifier, has to be either `iota` or `pres`
-        bspl (scipy.interpolate.BSpline): B-Spline object.
-        params (dict, optional): dictionary of gvec input parameters. Defaults to {}.
-
-    Raises:
-        AssertionError: If the profile type is not `iota` or `pres`.
-        TypeError: If `bspl` is not the expected scipy BSpline.
-
-    Returns:
-        dict: Dictionary of gvec input parameters
-    """
-
-    if which_profile not in ["iota", "pres"]:
-        raise AssertionError(
-            "Specified profile is not known!"
-            + "`which_profile` has to be either `iota` or `pres`."
-        )
-
-    if isinstance(bspl, BSpline):
-        params[which_profile + "_knots"] = np2gvec(bspl.t)
-        params[which_profile + "_coefs"] = np2gvec(bspl.c)
-        params[which_profile + "_type"] = "bspl"
-    else:
-        raise TypeError("Expected instance of scipy.interpolate.BSpline")
-    return params
-
-
 def bspl2gvec(
-    which_profile: str, knots: ArrayLike, coefs: ArrayLike, params: dict = {}
+    name: Literal["iota", "pres"],
+    bspl: BSpline = None,
+    knots: ArrayLike = None,
+    coefs: ArrayLike = None,
+    params: dict = {},
 ) -> dict:
-    """Translates B-spline coefficients and knots for either a iota or pressure profile into dictionary entries
+    """Translates a scipy B-spline object or B-spline coefficients and knots for either a iota or pressure profile into a dictionary entries
     that can be handed to `adapt_parameter_file`.
 
     Args:
-        which_profile (str): profile identifyer, has to be either `iota` or `pres`.
+        name (str): profile identifyer, has to be either `iota` or `pres`.
+        bspl (scipy.interpolate.BSpline): scipy BSpline object. If this is not provided `knots` and `coefs` are expected.
         knots (ArrayLike): Knots for the B-splines. Note that repeated edge knots according to the degree are expected.
         coefs (ArrayLike): Coefficients for the B-splines.
         params (dict, optional): Dictionary of gvec input parameters that will be adapted. Defaults to {}.
 
     Raises:
-        AssertionError: If `which_profile` is neither `iota` nor `pres`.
+        ValueError: If `name` is neither `iota` nor `pres`.
+        TypeError: If neither `bspl` nor `knots` and `coefs` is provided.
 
     Returns:
         dict: Dictionary of gvec input parameters
     """
-    if which_profile not in ["iota", "pres"]:
-        raise AssertionError(
+    if name not in ["iota", "pres"]:
+        raise ValueError(
             "Specified profile is not known!"
             + "`which_profile` has to be either `iota` or `pres`."
         )
+    if (bspl is None) and (knots is None or coefs is None):
+        raise TypeError(
+            "`bspl` and at least one of `knots` or `coefs` are None."
+            + "Please provide either `bspl` or `knots` and `coefs`"
+        )
 
-    params[which_profile + "_coefs"] = np2gvec(coefs)
-    params[which_profile + "_knots"] = np2gvec(knots)
-    params[which_profile + "_type"] = "bspline"
+    if bspl is not None:
+        params[f"{name}_coefs"] = np2gvec(bspl.c)
+        params[f"{name}_knots"] = np2gvec(bspl.t)
+    else:
+        params[f"{name}_coefs"] = np2gvec(coefs)
+        params[f"{name}_knots"] = np2gvec(knots)
+    params[f"{name}_type"] = "bspline"
 
     return params

test-CI/unit-pygvec/test_profiles.py
@@ -81,7 +81,7 @@ def c_poly():
     Returns:
         list: polynomial coefficients
     """
-    return [1, 2, 3, -1, -2, -3]
+    return [1, 0, 3, 1, 0, 1]
 
 
 @pytest.fixture(scope="module", params=["poly", "bspl"])
@@ -126,7 +126,7 @@ def test_eval_profile(teststate, type, c_poly):
     ref_poly = np.polynomial.Polynomial(c_poly)
     ref_poly_vals = ref_poly(rho**2)
     gvec_profile = teststate.evaluate_profile(type, rho=rho)
-    np.testing.assert_almost_equal(ref_poly_vals, gvec_profile)
+    np.testing.assert_allclose(gvec_profile, ref_poly_vals)
 
 
 @pytest.mark.parametrize("type", ["p_prime", "iota_prime"])
@@ -135,7 +135,7 @@ def test_eval_profile_prime(teststate, type, c_poly):
     ref_poly = np.polynomial.Polynomial(c_poly)
     ref_poly_vals = ref_poly.deriv(m=1)(rho**2) * 2 * rho
     gvec_profile = teststate.evaluate_profile(type, rho=rho)
-    np.testing.assert_almost_equal(ref_poly_vals, gvec_profile)
+    np.testing.assert_allclose(gvec_profile, ref_poly_vals)
 
 
 @pytest.mark.parametrize("type", ["p", "iota"])
@@ -157,9 +157,8 @@ def test_eval_profile_n_deriv(teststate, type, c_poly):
     dref1 = drefs1 * ds1
     dref2 = ds1**2 * drefs2 + drefs1 * ds2
     dref3 = 3 * ds1 * drefs2 * ds2 + ds1**3 * drefs3
-    dref4 = ds1**4 * drefs4 + 6 * ds2 * ds1**2 * drefs4 + 3 * ds2**2 * drefs2
-    drefi = [dref1, dref2, dref3, dref4]
+    dref4 = ds1**4 * drefs4 + 6 * ds2 * ds1**2 * drefs3 + 3 * ds2**2 * drefs2
 
-    for i in range(1, len(drefi)):
-        gvec_profile_di = teststate.evaluate_profile_deriv(type, rho=rho, order=i)
-        np.testing.assert_almost_equal(drefi[i - 1], gvec_profile_di)
+    for i, dref in enumerate([dref1, dref2, dref3, dref4]):
+        gvec_profile_di = teststate.evaluate_profile(type, rho=rho, deriv=i + 1)
+        np.testing.assert_allclose(dref, gvec_profile_di)