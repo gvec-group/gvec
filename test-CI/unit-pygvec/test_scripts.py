535359a17a40b86145a213f2f00c514fdc1d9801
Robert Babin <robert.babin@ipp.mpg.de>
2025-03-11 15:50:36 +0100

    add tests & docs

--- test-CI/unit-pygvec/test_scripts.py
+++ test-CI/unit-pygvec/test_scripts.py
@@ -34,68 +34,25 @@ def prepare_testcaserundir(tmp_path):
 # === TESTS === #
 
 
-def test_zero_current():
-    """
-    Test the zero_current script
-    """
-    from gvec.scripts.zero_current import run_zero_current
-
-    run_zero_current(
-        template="parameter.ini",
-        max_iteration=2,
-        gvec_miniter=5,
-        gvec_maxiter=10,
-        gvec_nelems_min=2,
-        gvec_nelems_max=2,
-        iota_poly_degree=5,
-        reinit_LA=False,
-        progressbar=False,
-        outputfile="GVEC_zero_current.nc",
-    )
-
-    for i in range(2 + 1):
-        assert Path(f"{i:02d}").exists()
-        assert Path(f"{i:02d}/parameter.ini").exists()
-        assert Path(f"{i:02d}/stdout.txt").exists()
-        assert len(list(Path(f"{i:02d}").glob("*State*.dat"))) == 2
-
-    assert list(Path("00").glob("*State*0000.dat"))
-    assert list(Path("00").glob("*State*0005.dat"))
-    assert list(Path("02").glob("*State*0000.dat"))
-    assert list(Path("02").glob("*State*0010.dat"))
-    assert Path("GVEC_zero_current.nc").exists()
-
-
-def test_zero_current_cmd():
+@pytest.mark.parametrize("suffix", ["ini", "yaml", "toml"])
+def test_zero_current_cmd(suffix):
     """
     Test the zero_current script via command line
     """
     from subprocess import run
 
-    run(
+    proc = run(
         [
-            "gvec_zero_current",
-            "parameter.ini",
-            "--pi",
-            "3",
-            "--gi",
-            "5",
-            "10",
-            "--ge",
-            "2",
-            "2",
-            "-q",
+            "pygvec",
+            "run",
+            f"parameter.{suffix}",
         ]
     )
+    assert proc.returncode == 0
 
-    for i in range(2 + 1):
-        assert Path(f"{i:02d}").exists()
-        assert Path(f"{i:02d}/parameter.ini").exists()
-        assert Path(f"{i:02d}/stdout.txt").exists()
-        assert len(list(Path(f"{i:02d}").glob("*State*.dat"))) == 2
-
-    assert list(Path("00").glob("*State*0000.dat"))
-    assert list(Path("00").glob("*State*0005.dat"))
-    assert list(Path("02").glob("*State*0000.dat"))
-    assert list(Path("02").glob("*State*0010.dat"))
-    assert Path("GVEC_zero_current.nc").exists()
+    if suffix == "ini":
+        assert Path("W7X_State_0000_00000100.dat").exists()
+    else:
+        assert Path("0-00/W7X_State_0000_00000025.dat").exists()
+        assert Path("0-01/W7X_State_0001_00000025.dat").exists()
+        assert Path("1-04/W7X_State_0007_00000005.dat").exists()
