fix winding number in surface computation

python/gvec/surface.py
@@ -39,7 +39,7 @@ def init_surface(
         "tor",
     }:
         raise ValueError(
-            "expected pos to be a DataArray with dimensions ('xyz', 'rad', 'pol', 'tor') or ('xyz', 'pol', 'tor')"
+            f"expected pos to be a DataArray with dimensions ('xyz', 'rad', 'pol', 'tor') or ('xyz', 'pol', 'tor'), not {pos.dims}"
         )
 
     if "rad" not in pos.dims:
@@ -71,7 +71,7 @@ def init_surface(
         zeta1d = pos.zeta_B
         theta2d, zeta2d = xr.broadcast(theta1d, zeta1d)
     if ift not in ["fft", "eval"]:
-        raise ValueError("expected ift to be 'fft', 'eval' or None")
+        raise ValueError(f"expected ift to be 'fft', 'eval' or None, not {ift}")
 
     surf = xr.Dataset(coords=pos.coords)
     surf["winding"] = ((), winding)
@@ -81,12 +81,10 @@ def init_surface(
     )
 
     for r in pos.rad:
-        xhat = np.cos(winding * zeta1d) * pos.sel(xyz="x", rad=r) + np.sin(
-            winding * zeta1d
-        ) * pos.sel(xyz="y", rad=r)
-        yhat = -np.sin(winding * zeta1d) * pos.sel(xyz="x", rad=r) + np.cos(
-            winding * zeta1d
-        ) * pos.sel(xyz="y", rad=r)
+        x = pos.sel(xyz="x", rad=r)
+        y = pos.sel(xyz="y", rad=r)
+        xhat = np.cos(winding * zeta1d) * x + np.sin(winding * zeta1d) * y
+        yhat = -np.sin(winding * zeta1d) * x + np.cos(winding * zeta1d) * y
         zhat = pos.sel(xyz="z", rad=r)
 
         # Ignore stellarator symmetry: will not store fourier coefficients - performance impact is negligible
@@ -168,13 +166,11 @@ def e_theta_B(ds: xr.Dataset):
             (
                 ds.dxhat_dt * np.cos(ds.winding * ds.zeta_B)
                 - ds.dyhat_dt * np.sin(ds.winding * ds.zeta_B)
-            )
-            * ds.winding,
+            ),
             (
                 ds.dxhat_dt * np.sin(ds.winding * ds.zeta_B)
                 + ds.dyhat_dt * np.cos(ds.winding * ds.zeta_B)
-            )
-            * ds.winding,
+            ),
             ds.dzhat_dt,
         ],
         dim="xyz",
@@ -197,10 +193,10 @@ def e_theta_B(ds: xr.Dataset):
 def e_zeta_B(ds: xr.Dataset):
     ds["e_zeta_B"] = xr.concat(
         [
-            (ds.dxhat_dz * ds.winding - ds.yhat) * np.cos(ds.winding * ds.zeta_B)
-            - (ds.dyhat_dz * ds.winding + ds.xhat) * np.sin(ds.winding * ds.zeta_B),
-            (ds.dxhat_dz * ds.winding - ds.yhat) * np.sin(ds.winding * ds.zeta_B)
-            + (ds.dyhat_dz * ds.winding + ds.xhat) * np.cos(ds.winding * ds.zeta_B),
+            (ds.dxhat_dz - ds.yhat * ds.winding) * np.cos(ds.winding * ds.zeta_B)
+            - (ds.dyhat_dz + ds.xhat * ds.winding) * np.sin(ds.winding * ds.zeta_B),
+            (ds.dxhat_dz - ds.yhat * ds.winding) * np.sin(ds.winding * ds.zeta_B)
+            + (ds.dyhat_dz + ds.xhat * ds.winding) * np.cos(ds.winding * ds.zeta_B),
             ds.dzhat_dz,
         ],
         dim="xyz",
@@ -259,13 +255,11 @@ def k_tt_B(ds: xr.Dataset):
             (
                 ds.dxhat_dtt * np.cos(ds.winding * ds.zeta_B)
                 - ds.dyhat_dtt * np.sin(ds.winding * ds.zeta_B)
-            )
-            * ds.winding,
+            ),
             (
                 ds.dxhat_dtt * np.sin(ds.winding * ds.zeta_B)
                 + ds.dyhat_dtt * np.cos(ds.winding * ds.zeta_B)
-            )
-            * ds.winding,
+            ),
             ds.dzhat_dtt,
         ],
         dim="xyz",

test-CI/unit-pygvec/test_surface.py
@@ -25,20 +25,22 @@ def ift(request):
 
 
 @pytest.fixture()
-def ev(state, ift):
+def ev(state):
     rho = [0.1, 0.5, 0.9]
-    if ift == "fft":
-        ds = EvaluationsBoozer(rho=rho, theta_B=21, zeta_B=51, state=state, MNfactor=1)
-    else:
-        ds = EvaluationsBoozer(rho=rho, theta_B=20, zeta_B=50, state=state, MNfactor=1)
+    ds = EvaluationsBoozer(rho=rho, theta_B=21, zeta_B=51, state=state, MNfactor=1)
     # only for testing! MNfactor should be ~4 or above for most applications
     compute(ds, "pos", "N_FP", state=state)
     return ds
 
 
+@pytest.fixture(params=[0, 1, -1])
+def winding(request, state):
+    return 1 + request.param * state.nfp
+
+
 @pytest.fixture()
-def surfs(ev, ift):
-    return surface.init_surface(ev.pos, ev.N_FP, ift=ift)
+def surfs(ev, ift, winding):
+    return surface.init_surface(ev.pos, ev.N_FP, ift=ift, winding=winding)
 
 
 # === TESTS === #
@@ -82,15 +84,23 @@ def test_compute(surfs, Q):
     assert np.isnan(surfs[Q].data).sum() == 0
 
 
+@pytest.mark.parametrize("ift", ["fft"])
 @pytest.mark.parametrize(
     "Q",
     [
         "pos",
+        "e_theta_B",
+        "e_zeta_B",
+        "g_tt_B",
+        "g_tz_B",
+        "g_zz_B",
+        "normal",
     ],
 )
-def test_compare(state, ev, surfs, Q):
+def test_compare(state, ev, surfs, Q, ift):
     """Test that the computed surface quantities match the expected values."""
     surface.compute(surfs, Q)
     compute(ev, Q, state=state)
 
-    assert np.allclose(surfs[Q].data, ev[Q].data)
+    surfQ, evQ = xr.broadcast(surfs[Q], ev[Q])
+    np.testing.assert_allclose(surfQ.data, evQ.data, rtol=1e-2, atol=1e-5)