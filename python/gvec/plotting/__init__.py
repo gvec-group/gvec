85ce6f2b295381d4f4fd0b42e82b4061aba45673
mude <dean.muir@ipp.mpg.de>
2025-12-11 11:31:55 +0100

    Added `usetext` to the `__init__.py` and removed it from other files. This line needs to be run once on import.
    Missed in previous commit.

--- python/gvec/plotting/__init__.py
+++ python/gvec/plotting/__init__.py
@@ -1,3 +1,7 @@
+import matplotlib.pyplot as plt
+
 from .plots1d import plot_on_axis, plot_radial_profile
 from .plots2d import plot_on_flux_surface, plot_poloidal_plane
 from .plots3d import plot_3d_surface, plot_boundary
+
+plt.rcParams.update({"text.usetex": True})
