add twist computation. fix derivations
* twist computed from curve + linear independent direction. Using dft for first and second derivatives.
* test for linking number from boundary test case added
* test for writhe from two curves of boundary test case, comparison possible of Wr=Lk-Tr, to approximate writhe from double integral (200 points per field period, within accuracy limit of 1e-3).

python/gvec/gframe.py
@@ -13,7 +13,7 @@ import numpy as np
 import xarray as xr
 from scipy.optimize import root_scalar
 
-from gvec.util import logging_setup, write_parameters
+from gvec.util import logging_setup, write_parameters, linking_number
 from gvec import fourier
 
 
@@ -703,6 +703,12 @@ def construct_gframe_from_surface(
         xyz_surf = xyz_hat_to_xyz(xhatfull, yhatfull, zhatfull, zetafull, sign_rot)
 
     logger.info(". Constructing the G-Frame")
+    # check linking number of the surface (curves at theta=0 and theta=pi):
+    Lk = linking_number(xyz_surf[:, 0, :], xyz_surf[:, nt // 2, :])
+    assert np.abs(Lk - np.rint(Lk)) < 1e-8, "Linking number of the surface is not integer!"
+    logger.info(
+        f"  - linking number of the surface = {Lk:.0f} (number of poloidal windings around its center)"
+    )
 
     if cutoff_gframe < 0:
         nz_gframe = nz
@@ -1100,3 +1106,68 @@ def plot_cross_section_comparison(dict_surf, dict_RZ, step=1, halfperiod=True):
         label=r"$\zeta/(2\pi/N_{FP})$",
     )
     return fig
+
+
+def twist_of_ribbon(X0: np.ndarray, N: np.ndarray, nint: int = None) -> float:
+    r"""
+    Compute the twist of a closed (!) ribbon defined by a centerline curve $X_0(\zeta)$ and non-vanishing "normal" vector $N(\zeta)$. The vector N only needs to be linearly independent of the tangent vector $|X_0'(\zeta) \times N| >0$
+
+    The twist is computed as
+
+    $$
+    \text{Tw} = \frac{1}{2\pi}\int_0^{2\pi}\frac{\left ({N\times \left [{N^\prime\left|{\Xp}\right|^2 - \left({N \cdot X_0^\prime}\right) X_0^{\prime\prime}}\right]}\right) \cdot X_0^\prime}{\left|{N\left|{X_0^\prime}\right|^2-\left({N \cdot X_0^\prime}\right) X_0^\prime}\right|^2}\left|{X_0^\prime}\right| d\zeta
+    $$
+
+    Derivatives of $X_0$ and $N$ are computed via fft, so the curve is assumed to be given on an equispaced grid in `zeta=np.linspace(0,2*np.pi,npoints,endpoint=False)`, excluding the endpoint.
+
+    Parameters
+    ----------
+    X0 : np.ndarray
+        curve positions, in cartesian coordinates, shape (npoints,3),  excluding periodic endpoint!
+    N  : np.ndarray
+        linearly independent, non-normalized "normal" vector at curve positions, in cartesian coordinates, same shape as X0
+    nint : int, optional
+        number of points for integration, None sets default =npoints
+
+    Returns
+    -------
+    Tw : float
+        twist of the ribbon defined by X0 and N
+    """
+    nzeta = X0.shape[0]
+    assert nzeta == N.shape[0], (
+        f"X0 and N must have the same length, but are {X0.shape[0]} and {N.shape[0]} respectively"
+    )
+    assert X0.shape[1] == 3, f"X0 must have shape (npoints,3), but has shape {X0.shape}"
+    assert N.shape[1] == 3, f"X0 must have shape (npoints,3), but has shape {N.shape}"
+    assert np.sqrt(np.sum((X0[0, :] - X0[-1, :]) ** 2)) > 1e-8, (
+        "X0 must exclude endpoint, but first and last point coincide"
+    )
+    zeta = np.linspace(0, 2 * np.pi, nzeta, endpoint=False)
+    nint_zeta = nint if nint is not None else nzeta
+    zeta_int = np.linspace(0, 2 * np.pi, nint_zeta, endpoint=False)
+    dft = fourier.real_dft_mat(zeta, zeta_int)
+    dft_BF_dz = (fourier.get_B_dft(dft["x_out"], 1, dft["nfp"], dft["modes"]) @ dft["F"]).real
+    dft_BF_dzdz = (fourier.get_B_dft(dft["x_out"], 2, dft["nfp"], dft["modes"]) @ dft["F"]).real
+    if nint is None:
+        N_f = N
+    else:
+        N_f = dft["BF"] @ N
+    Xp = dft_BF_dz @ X0
+    N_x_Xp = np.sqrt(np.sum((np.cross(N_f, Xp)) ** 2, axis=-1))
+    N_dot_Xp = np.vecdot(N_f, Xp)
+    N_Xp_angle = np.atan2(N_x_Xp, N_dot_Xp)
+    assert np.all(N_Xp_angle > 1e-8), (
+        f"N must be linearly independent of Xp, min angle={np.amin(N_Xp_angle)}"
+    )
+
+    Xpp = dft_BF_dzdz @ X0
+    Np = dft_BF_dz @ N
+
+    # cross and vecdot apply only on last axis!
+    Xp_dot_Xp = np.vecdot(Xp, Xp)
+
+    num = np.vecdot(np.cross(N_f, (Np * Xp_dot_Xp[:, None] - N_dot_Xp[:, None] * Xpp)), Xp)
+    denom = np.sum((N_f * Xp_dot_Xp[:, None] - N_dot_Xp[:, None] * Xp) ** 2, axis=-1)
+    # intergate over zeta and divide by 2pi, using trapezoidal rule
+    return np.average(num / denom * np.sqrt(Xp_dot_Xp))

python/gvec/util.py
@@ -983,7 +983,9 @@ def compute_FD(f: np.ndarray, pos, coefs, axis=0):
         axis  : axis along which the FD is computed, default is 0
     Returns:
         df    : Finite-Difference result, same shape as f (see warning above!)
+
     Examples:
+
     - examples for first derivative of f:
         - 1st order forward FD: `pos=[1,0]; coefs=[-1,1]/(dx)`
         - 2nd order central FD: `pos=[-1,1]; coefs=[-1,1]/(2*dx)`
@@ -1207,31 +1209,53 @@ def boundary_generator(case: str, X1_00=1.0, a0=0.5, ellipticity=0.4, helix_r=0.
     return params
 
 
-def linking_number(curve_a: np.ndarray, curve_b: np.ndarray, tol=1e-15):
-    """
+def linking_number(curve_a: np.ndarray, curve_b: np.ndarray, tol=1e-15, endpoint=True):
+    r"""
     Compute the linking number of two non-intersecting curves $C_a(\zeta_a),C_b(\zeta_b)$, solving the (non-singular) double integral over two curves
 
-    $$\textrm{Lk} = \Lk = \frac{1}{4\pi}\int_0^{2\pi} \int_0^{2\pi} \frac{ r_b - r_a}{\abs{r_b - r_a}^3} \cdot \left(\frac{\partial r_a}{\zeta_a}  \times \frac{\partial r_b}{\zeta_b}} d{\zeta_a} d{\zeta_b}$$
+    $$
+    \text{Lk} = \frac{1}{4\pi}\int_0^{2\pi} \int_0^{2\pi} \frac{ r_b - r_a}{\abs{r_b - r_a}^3} \cdot \left(\frac{\partial r_a}{\partial\zeta_a}  \times \frac{\partial r_b}{\partial\zeta_b}\right) d{\zeta_a} d{\zeta_b}
+    $$
 
     Here, the double integral is approximated by representing the curves as polygons, and computing the linking number as the sum of solid angles between all linear segments of polygon curves. The solid angle of two linear segments can be computed exactly. See paper by  [Klenin and Langowski](https://onlinelibrary.wiley.com/doi/10.1002/1097-0282(20001015)54:5%3C307::AID-BIP20%3E3.0.CO;2-Y)
     See GVEC documentation for more details.
-    Args:
-        curve_a (2D np.ndarray): the x,y,z point positions of the first curve. First and last point must coincide. shape must be [npoints_a,3]
-        curve_b (2D np.ndarray): the x,y,z point positions of the first curve. First and last point must coincide. shape must be [npoints_b,3]
-        tol (float): tolerance to consider points coincident (default: 1e-15)
-    Returns:
-        Lk (float): the linking number of the two curves
+
+    Parameters
+    ----------
+    curve_a : np.ndarray
+        the x,y,z point positions of the first curve. First and last point must coincide, if endpoint=True. shape must be [npoints_a,3]
+    curve_b : np.ndarray
+        the x,y,z point positions of the first curve. First and last point must coincide, if endpoint=True. shape must be [npoints_b,3]
+    tol : float
+        tolerance to consider points coincident (default: 1e-15)
+    endpoint : bool
+        `True`:  the first and last point of each curve coincide (default), else the last point is connected to the first point to close the curve.
+
+    Returns
+    -------
+    Lk : float
+        the linking number of the two curves
     """
     assert curve_a.ndim == 2, "curve_a must be a 2D array"
     assert curve_b.ndim == 2, "curve_b must be a 2D array"
     assert curve_a.shape[1] == 3, "second dimension of curve_a must be of size 3"
     assert curve_b.shape[1] == 3, "second dimension of curve_b must be of size 3"
-    assert np.allclose(curve_a[0, :], curve_a[-1, :]), (
-        "first and last point of curve_a must coincide (closed curve)"
-    )
-    assert np.allclose(curve_b[0, :], curve_b[-1, :]), (
-        "first and last point of curve_b must coincide (closed curve)"
-    )
+    closed_a = np.allclose(curve_a[0, :], curve_a[-1, :])
+    closed_b = np.allclose(curve_b[0, :], curve_b[-1, :])
+    if endpoint:
+        assert closed_a, "first and last point of curve_a must coincide (closed curve)"
+        assert closed_b, "first and last point of curve_b must coincide (closed curve)"
+        _curve_a = curve_a
+        _curve_b = curve_b
+    else:
+        assert not closed_a, (
+            "first and last point of curve_a coincide, but endpoint=False was chosen"
+        )
+        assert not closed_b, (
+            "first and last point of curve_b coincide, but endpoint=False was chosen"
+        )
+        _curve_a = np.vstack([curve_a, curve_a[0, :]])
+        _curve_b = np.vstack([curve_b, curve_b[0, :]])
     # nseg_a = curve_a.shape[0]-1
     # nseg_b = curve_b.shape[0]-1
     # Lk = 0.0
@@ -1245,41 +1269,55 @@ def linking_number(curve_a: np.ndarray, curve_b: np.ndarray, tol=1e-15):
     # Lk /= (4.0*np.pi)
     return np.sum(
         solid_angle_between_segments(
-            curve_a[0:-1, None, :],
-            curve_a[1:, None, :],
-            curve_b[None, 0:-1, :],
-            curve_b[None, 1:, :],
+            _curve_a[0:-1, None, :],
+            _curve_a[1:, None, :],
+            _curve_b[None, 0:-1, :],
+            _curve_b[None, 1:, :],
             tol=tol,
         )
     ) / (4 * np.pi)
 
 
-def writhe(curve: np.ndarray):
-    """
-    Compute the writhe of a curve $C(\zeta)$, solving the (singular!) double integral over the curve
+def writhe(curve: np.ndarray, endpoint=True):
+    r"""
+    Compute the writhe of a closed curve $C(\zeta)$, solving the (singular!) double integral over the curve
 
-    $$\textrm{Wr} = \Wr = \frac{1}{4\pi}\int_0^{2\pi} \int_0^{2\pi} \frac{ r(\zeta') - r(\zeta)}{\abs{r(\zeta') - r(\zeta)}^3} \cdot \left(\frac{\partial r}{\zeta}  \times \frac{\partial r}{\zeta'}} d{\zeta} d{\zeta'}$$
+    $$
+    \text{Wr} = \frac{1}{4\pi}\int_0^{2\pi} \int_0^{2\pi} \frac{ r(\zeta^\prime) - r(\zeta)}{\abs{r(\zeta^\prime) - r(\zeta)}^3} \cdot \left(\frac{\partial r}{\partial\zeta}  \times \frac{\partial r}{\partial\zeta^\prime} \right) d{\zeta} d{\zeta^\prime}
+    $$
 
     Here, the double integral is approximated by representing the curve as a polygon, and computing the writhe as the sum of solid angles between all linear segments of polygon curve. The solid angle of two linear segments can be computed exactly. See paper by  [Klenin and Langowski](https://onlinelibrary.wiley.com/doi/10.1002/1097-0282(20001015)54:5%3C307::AID-BIP20%3E3.0.CO;2-Y)
     See GVEC documentation for more details.
-    Args:
-        curve (2D np.ndarray): the x,y,z point positions of the curve. First and last point must coincide. shape must be [npoints,3]
-    Returns:
-        Wr (float): the approximate writhe of the curve
+
+    Parameters
+    ----------
+    curve : np.ndarray
+        the x,y,z point positions of the curve. First and last point must coincide, if endpoint=True. shape must be [npoints,3]
+    endpoint : bool
+        `True`: the first and last point of the curve coincide (default), else the last point is connected to the first point to close the curve.
+
+    Returns
+    -------
+    Wr : float
+        the approximate writhe of the curve
     """
     assert curve.ndim == 2, "curve must be a 2D array"
     assert curve.shape[1] == 3, "second dimension of curve must be of size 3"
-    assert np.allclose(curve[0, :], curve[-1, :]), (
-        "first and last point of curve must coincide (closed curve)"
-    )
-    nseg = curve.shape[0] - 1
+    if endpoint:
+        assert np.allclose(curve[0, :], curve[-1, :]), (
+            "first and last point of curve must coincide (closed curve)"
+        )
+        _curve = curve
+    else:
+        _curve = np.vstack([curve, curve[0, :]])
+    nseg = _curve.shape[0] - 1
     # mask to select only pairs of segments (i,j) with j>i+1 to avoid coincident or adjacent segments
     mask = np.triu(np.ones((nseg, nseg), dtype=np.int8), k=2)
     # apply mask to segment(i) x segment(j), with j>i+1 remaining
-    r1 = (curve[0:-1, None, :] * mask[:, :, None])[(mask == 1), :]
-    r2 = (curve[1:, None, :] * mask[:, :, None])[(mask == 1), :]
-    r3 = (curve[None, 0:-1, :] * mask[:, :, None])[(mask == 1), :]
-    r4 = (curve[None, 1:, :] * mask[:, :, None])[(mask == 1), :]
+    r1 = (_curve[0:-1, None, :] * mask[:, :, None])[(mask == 1), :]
+    r2 = (_curve[1:, None, :] * mask[:, :, None])[(mask == 1), :]
+    r3 = (_curve[None, 0:-1, :] * mask[:, :, None])[(mask == 1), :]
+    r4 = (_curve[None, 1:, :] * mask[:, :, None])[(mask == 1), :]
 
     # factor 2 to account for symmetry of (i,j) and (j,i)
     return np.sum(solid_angle_between_segments(r1, r2, r3, r4)) / (2 * np.pi)