f7d2b243861ffdcb28177dd63c828e01cca51de2
Florian Hindenlang <florian.hindenlang@ipp.mpg.de>
2026-04-14 10:56:35 +0200

    check for negative Jacobian of hmap at initialization.
    * G-Frame initialization now checks for T.(N x B)>0 at the points of the origin curve X0.

--- test-CI/unit-pygvec/test_scripts.py
+++ test-CI/unit-pygvec/test_scripts.py
@@ -99,6 +99,10 @@ def test_run_recover_from_error():
     parameters["X1_b_cos"][1, 0] = -1.0
     with pytest.raises(gvec.errors.InitializationError):
         gvec.run(parameters)
+    parameters = gvec.util.read_parameters("parameter.ini")
+    parameters["X1_a_cos"][0, 0] = 0.1
+    with pytest.raises(gvec.errors.InitializationError):
+        gvec.run(parameters)
 
     parameters = gvec.util.read_parameters("parameter.ini")
     parameters["maxIter"] = 1
